b024937b241a055f346f5487c5a192fefd4ded80
morganstaff <morganstaffwarner@gmail.com>
2017-08-22 17:06:07 -0400

    Added consenant

--- third.py
+++ third.py
@@ -8,5 +8,7 @@ consenants = 'bcdfghjklmnpqrstvwxyz'
 
 if l in vowels:
   print "{l} is a vowel".format(**locals())
+elif l in consenants:
+  print "{l} is a c-c-c-consenant.".format(**locals())
 else:
   print "{l} is nothing!".format(**locals())
