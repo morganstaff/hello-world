4a767895b9797eb15baade4d182a81e59e3966c1
morganstaff <morganstaffwarner@gmail.com>
2017-08-22 17:17:11 -0400

    Added nums

--- third.py
+++ third.py
@@ -6,9 +6,13 @@ vowels = ['a', 'e', 'i', 'o', 'u']
 
 consenants = 'bcdfghjklmnpqrstvwxyz'
 
+nums = '1234567890'
+
 if l in vowels:
   print "{l} is a vowel".format(**locals())
 elif l in consenants:
   print "{l} is a c-c-c-consenant.".format(**locals())
+elif l in nums:
+  print "That's not a letter! That's a single digit.".format(**locals())
 else:
   print "{l} is nothing!".format(**locals())
