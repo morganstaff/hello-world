da780168951602f590bfc64049966a60fe2b1128
morganstaff <morganstaffwarner@gmail.com>
2017-08-22 22:41:00 -0400

    Adding some more funk.

--- funk.py
+++ funk.py
@@ -1,8 +1,18 @@
 #!/bin/python
 
+
+
 def say_hello():
   print "Hello"
+
+def greeting(name, what):
+  print "Hello {name}!\nThis is what I'm supposed to say:\n\t{what}".format(**locals())
 
 print "Call function hello"
 
 say_hello()
+
+message = raw_input("What do you want to me to say?")
+name = raw_input("Who am I saying this to?")
+
+greeting(name, message)
